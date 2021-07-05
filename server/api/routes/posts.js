bc60e7125aef39a02de312de284c7f4aad2ada5d
Tadeo Cuchman <sifra@192.168.1.2>
2021-07-04 22:54:43 -0300

    rutas de mi servidor

--- server/api/routes/posts.js
+++ server/api/routes/posts.js
@@ -1,45 +1,72 @@
 const express = require('express')
+const { verifyToken, hasRoles } = require('../middlewares/jwt-validate')
+const user = require('./users')
 
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
+
     res.json({ success: true, message: 'Feed:', Posts}).status(200)
 })
 
-router.get('/:id', (req, res) => {
-    Post.findById(req.params.id)
-        .exec()
-        .then(x => res.status(200).send(x))
+router.get('/npages', (req, res) => {
+    const nPages = (Posts.length) / 10
+    const nRedondeado = Math.round(nPages)
+    res.json({ success: true, message: 'Number of pages:', nRedondeado}).status(200)
 })
 
-router.post('/', (req, res) => {
+router.get('/page', (req, res) => {
+    const page = req.query.page;
+
+    const firstIndex = (page - 1) * 10
+    const lastIndex = firstIndex + 10
+    const postPage = Posts.slice(firstIndex, lastIndex)
+
+    res.json({ success: true, message: 'Pagina del Feed:', postPage}).status(200)
+})
+
+router.get('/category', (req, res) => {
+    const category = req.query.category;
+
+    const filtrados = []
+    Posts.forEach((x) => {
+        if (x.category == category)  
+            filtrados.push(x)      
+    })
+    res.json({succes: true, message:'Elementos de misma categoria', filtrados})
+})
+
+router.post('/', verifyToken, (req, res) => {
     const existeAlbum = Posts.find((a) => {
-        req.body.album === a.album
+       return req.body.album === a.album
     })
     if (existeAlbum) {
         return res.status(400).json({ success: false, message: 'Album ya existe.'})
     }
+    else {
     const newPost = {
         category: req.body.category,
         link: req.body.link,
         album: req.body.album,
         band: req.body.band,
-        description: req.body.description
-    }
-    Posts.push(newPost);
-    res.json({success:'true', newPost, Posts})
+        description: req.body.description,
+        firma: req.body.firma
+        }
+    Posts.unshift(newPost);
+    res.json({success: true, newPost, Posts})
+    }   
 })
 
-router.put('/:id', (req, res) => {
-    Post.findOneAndUpdate(req.params.id, req.body)
-        .then(() => res.sendStatus(204))
+router.put('/:id', verifyToken, (req, res) => {
+  
 })
 
-router.delete('/:id', (req, res) => {
-    Post.findByIdAndDelete(req.params.id).exec().then(() => res.sendStatus(204))
+router.delete('/', verifyToken, (req, res) => {
+    const category = req.query.category;
+    
 })
 
 const Posts = [];
 
-module.exports = router, Posts;
+module.exports = router;
