d4427ccab6ae920bbf4d9f5d12be4a7be6245be6
Tadeo Cuchman <sifra@192.168.1.2>
2021-08-06 21:28:14 -0300

    arreglo de numeritos en el foro

--- React/foromusic/src/Pages/Forum.jsx
+++ React/foromusic/src/Pages/Forum.jsx
@@ -12,19 +12,18 @@ const Forum = () => {
     const [popup, cambiarPopup] = useState(false)
     const [posts, cambiarPosts] = useState([])
     const [page, cambiarPage] = useState(1)
-    const [numeritos, cambiarNumeritos] = useState(1)
+    const [numeritos, cambiarNumeritos] = useState(0)
+    const [selected, cambiarSelected] = useState(0)
+    //hacer que el selected tenga un id de post seleccionado y este cambie de clase *= que se haga mas grande y con mas informacion mostrada
     
     useEffect(() => {
         cargarPosts()
-    }, [])
+    }, [page])
 
     const cargarPosts = () => {
         fetch('http://localhost:4000/feed/npages')
             .then(response => response.json())
-            .then(data => {  cambiarNumeritos(data.nRedondeado) 
-            
-                console.log(numeritos)
-            })
+            .then(data => {  cambiarNumeritos(data.nRedondeado) })
 
         fetch(`http://localhost:4000/feed/page/?page=${page}`)
             .then(response => response.json())
@@ -33,15 +32,44 @@ const Forum = () => {
 
     const Numerito = (props) => {
         return (
-            <button onClick={ cambiarPage(props.numerito)}>{props.numerito}</button>
+            <button id='numeritos' onClick={() => cambiarPage(props.numerito) }> {props.numerito} </button>
         )
-    
+    }
+
+    const BotonAntYSig = () => {
+        return (
+            <div>
+                <button onClick={()=>{
+                    if (page > 1){
+                        (cambiarPage(page-1))
+                    }
+                }}>prev</button>
+                
+                <button onClick={()=>{
+                    if (page < numeritos){
+                        cambiarPage(page+1)
+                    }
+
+                }}>next</button>
+
+               
+        
+
+            </div>
+        )
     }
     
-    const Numeritos = () => {
-        for (let i = 0; i < numeritos; i++){
-            return (<Numerito numerito={i} />)
+    const Numeritos = () => {   
+        let componentesNumeritos = []
+        let componentesMuchosNum = []
+        for (let i = 1; i < numeritos + 1; i++) {
+            componentesNumeritos.push( <Numerito numerito={i} key={i}/> );
         }
+        if (componentesNumeritos.length > 10){
+            componentesMuchosNum = [componentesNumeritos[0], componentesNumeritos[1], componentesNumeritos[2], '...', componentesNumeritos[page],'...' , componentesNumeritos[componentesNumeritos.length-1]]
+            return (componentesMuchosNum)
+        } 
+        return (componentesNumeritos)
     }
 
 
@@ -64,6 +92,9 @@ const Forum = () => {
         <ListaDePosts posts = {posts} />
 
         <Numeritos />
+        <BotonAntYSig />
+        <br />
+
 
         </main>
     )
