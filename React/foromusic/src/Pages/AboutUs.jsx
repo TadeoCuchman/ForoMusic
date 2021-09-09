50a8c8f0f53aa9f3416199e985861f5047d1f1b1
Tadeo Cuchman <sifra@192.168.1.4>
2021-09-09 01:19:16 -0300

    cambiar descripcion de la ap y el concepto

--- React/foromusic/src/Pages/AboutUs.jsx
+++ React/foromusic/src/Pages/AboutUs.jsx
@@ -3,19 +3,35 @@ import React from 'react';
 const AboutUs = () => {
     return (
            <main>
+                <br />
+               <h3>Concept:</h3>
+                <p>
+                    Every post is ordened by album! Easy :)
+                    <br/>
+                    <span>If you have done a post, any time someone cick on your post you make a point, simple! </span>
+                </p>
+               
+               <p>
+                   Meet all the albums who people really needed to share and talk about. 
+                   Even you can share and see what other people think about your best music.
+                   Always respecting others opinions, storys or shares about, obviusly.
+                    Anyways, 
+                   Have Fun, Discover new words!
+                </p>
+    
+              
                <br />
                <h3>Our vision:</h3>
                <p>
-                   We know that life is music. We honor life giving to it a little extra; reciving the rights vibrations by the best music.
+                   We understand that life is music. We honor life giving to it a little extra; by the best music.
                 </p>
                 <p>
-                   We create a community that love they hear, so me connect to create a diccionary of albums that need to stay forever on ours thoughs.
-                   So basicaly we invest on the construction of a big mind who enjoyed songs divectly from the human soul. Without any judgment we accept any 
-                   taste for the sound itself. Expression is the force of our world and me walk because of all the people who let us enjoy that part that we all 
-                   experience: The connetion itself of senses.
+                   We create a community that love what they hear, so me connect to create a diccionary of albums that need to stay forever on ours thoughs.
+                   So basicaly we invest on the construction of a big mind who enjoy songs directly from the human soul. Without any judgment we accept any 
+                   taste for the sound itself. Let's make the experience: The connetion of senses.
                 </p>
                 <p>
-                    We invite you to meet other worlds, create conections with others and with your newest self at any time. 
+                    We invite you to meet other worlds, create conections with others and with a newest self at any time. 
                 </p>
            </main>
         );
