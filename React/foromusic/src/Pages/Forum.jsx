4b0f884556a63d77c716e52bda34fe049c10221a
Tadeo Cuchman <sifra@192.168.1.4>
2021-09-09 01:35:14 -0300

    pequenas mejoras en el rendimiento del foro (falta sacar componentes Numeitos y BotonYSig para afuera para que otras paginas lo puedas usar)

--- React/foromusic/src/Pages/Forum.jsx
+++ React/foromusic/src/Pages/Forum.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { useState, useEffect } from "react";
-import ListaDePosts from '../Components/ListaDePosts';
-import Popup from '../Components/Popup';
-
-
-
+import React from 'react'
+import { useState, useEffect } from "react"
+import ListaDePosts from '../Components/ListaDePosts'
+import Popup from '../Components/Popup'
+import Searcher from '../Components/Searcher'
+import './Forum.css'
 
 
 
@@ -14,7 +13,7 @@ const Forum = () => {
     const [page, cambiarPage] = useState(1)
     const [numeritos, cambiarNumeritos] = useState(0)
 
-    
+
     useEffect(() => {
         cargarPosts()
     }, [page])
@@ -22,7 +21,7 @@ const Forum = () => {
     const cargarPosts = () => {
         fetch('http://localhost:4000/feed/npages')
             .then(response => response.json())
-            .then(data => {  cambiarNumeritos(data.nRedondeado) })
+            .then(data => { cambiarNumeritos(data.nRedondeado) })
 
         fetch(`http://localhost:4000/feed/page/?page=${page}`)
             .then(response => response.json())
@@ -32,68 +31,61 @@ const Forum = () => {
 
     const Numerito = (props) => {
         return (
-            <button className='numeritos' onClick={() => cambiarPage(props.numerito) }> {props.numerito} </button>
+            <button onClick={() => cambiarPage(props.numerito)}> {props.numerito} </button>
         )
     }
 
     const BotonAntYSig = () => {
         return (
-            <div>
-                <button className='antSig' onClick={()=>{
-                    if (page > 1){
-                        (cambiarPage(page-1))
+            <div className='antSig'>
+                <button onClick={() => {
+                    if (page > 1) {
+                        (cambiarPage(page - 1))
                     }
                 }}>prev</button>
-                
-                <button className='antSig' onClick={()=>{
-                    if (page < numeritos){
-                        cambiarPage(page+1)
+
+                <button onClick={() => {
+                    if (page < numeritos) {
+                        cambiarPage(page + 1)
                     }
 
                 }}>next</button>
-
-               
-        
-
             </div>
         )
     }
-    
-    const Numeritos = () => {   
+
+    const Numeritos = () => {
         let componentesNumeritos = []
         let componentesMuchosNum = []
         for (let i = 1; i < numeritos + 1; i++) {
-            componentesNumeritos.push( <Numerito numerito={i} key={i}/> );
+            componentesNumeritos.push(<Numerito numerito={i} key={i} />);
         }
-        if (componentesNumeritos.length > 10){
-            componentesMuchosNum = [componentesNumeritos[0], componentesNumeritos[1], componentesNumeritos[2], '...', componentesNumeritos[page],'...' , componentesNumeritos[componentesNumeritos.length-1]]
-            return (componentesMuchosNum)
-        } 
-        return (<div>{componentesNumeritos}</div>)
+        if (componentesNumeritos.length > 10) {
+            componentesMuchosNum = [componentesNumeritos[0], componentesNumeritos[1], componentesNumeritos[2], '...', componentesNumeritos[page], '...', componentesNumeritos[componentesNumeritos.length - 1]]
+            return (<div className='numeritos' >{componentesMuchosNum}</div>)
+        }
+        return (<div className='numeritos'>{componentesNumeritos}</div>)
     }
 
 
-    
 
-    return ( 
+
+    return (
         <main>
-        
-        <Popup mostrar= { popup } setMostrar = { cambiarPopup } cargarPosts = { cargarPosts } />
-        
-        <button id="nuevoArtBoton" onClick = {() => cambiarPopup(true)}> + </button>
 
-        <button id="delete" onClick = {() => {
+            <Popup mostrar={popup} setMostrar={cambiarPopup} cargarPosts={cargarPosts} />
 
-        }}>-</button>
+            <Searcher />
+            <button id="nuevoArtBoton" onClick={() => cambiarPopup(true)}> + </button>
 
 
-        <h1>FEED:</h1>
-        
-        <ListaDePosts posts = {posts} />
 
-        <Numeritos />
-        <BotonAntYSig />
-        <br />
+            <h1>FEED:</h1>
+            <ListaDePosts posts={posts} />
+
+            <Numeritos />
+            <BotonAntYSig />
+            <br />
 
 
         </main>
