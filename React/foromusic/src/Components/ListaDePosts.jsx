eaf84cef5654278646a9be67c633c3db4581db56
Tadeo Cuchman <sifra@192.168.1.4>
2021-09-09 01:21:22 -0300

    Lista de Post Muestra mas info y se mejoro la vista de los posts.

--- React/foromusic/src/Components/ListaDePosts.jsx
+++ React/foromusic/src/Components/ListaDePosts.jsx
@@ -1,54 +1,75 @@
 import React from 'react';
 import {useState} from 'react';
+import { Link } from "react-router-dom";
 
-//hacer que el selected tenga un id de post seleccionado y este cambie de clase *= que se haga mas grande y con mas informacion mostrada
+
 // agregar que al click despliegue mas informacion y la posibilidad de agregar comentarios
-// hacer que los post hagan una busqueda a youtube y despliegue informacion como una foto
+// hacer un grid y ordenar mejor el post
 
-
-const ListaDePosts = (props) => {
-    const posts = props.posts
-    const [selected, cambiarSelected] = useState(-1)
-
-    return (
-        <div id='feed'>
-            { posts.map((post) => {
-                return ( <Post
-                    id = {post.id}
-                    key = {post.id}
-                    category={post.category} 
-                    link={post.link}
-                    album={post.album}
-                    band={post.band}
-                    description={post.description}
-                    firm={post.firm}
-                    selected={post.id === selected}
-                    cambiarSelected={cambiarSelected}
-                    />
-                )
-            }) }
-        </div>
-    );
-}
 
 const Post = (props) => {
 
     return (
         <div className={` nuevoArticulo ${props.selected ? 'nuevoArticuloExpandido' : ''}`}
-        onClick={() => props.cambiarSelected(props.id)}>
-            <br />
-            <p>{props.category}</p>
-            <h3>{props.link}</h3>
-            <p>{props.album}</p>
-            {props.selected && 
-            <>
-            <p>{props.band}</p>
-            <p>{props.description}</p>
-            <p>{props.firm}</p>
-            </>}
-            <br />
+        onClick={() => { if (props.selected) {props.cambiarSelected(-1)} else {props.cambiarSelected(props.id)} }}>
+            <div className='title'>
+                <p>{props.album}</p>
+                <p>Band: {props.band}</p>
+            </div>
+           
+            <div className='info'>
+            
+                 {props.selected && 
+                <>
+                    <h3><a href={`${props.link}`} target="_blank" >{props.link}</a></h3>
+                    <p>Album date: {props.album_date}</p>
+                    <p>Category: {props.category}</p>
+                    <p>{props.description}</p>
+                    <div className='comments'>
+                        <Link to={`/Post/?:${props.id}`} id='comments' >Comments</Link>    
+                    </div>        
+                    <Link id='firm' to={`/User/:${props.user_id}`}>{props.firm}</Link>
+                 </>}
+            </div>
+           
+                
+            
+            
+            
+            
         </div>
     )
 }
+
+const ListaDePosts = (props) => {
+    const posts = props.posts
+    const [selected, cambiarSelected] = useState(-1)
+
+    if (posts){
+        return (
+            <div id='feed'>
+                { posts.map((post) => {
+                    return ( <Post
+                        id={post.id}
+                        user_id={post.user_id}
+                        key={post.id}
+                        category={post.category} 
+                        link={post.link}
+                        album={post.album}
+                        band={post.band}
+                        description={post.description}
+                        firm={post.firm}
+                        albumDate={post.album_date}
+                        selected={post.id === selected}
+                        cambiarSelected={cambiarSelected}
+                        />
+                    )
+                }) }
+            </div>
+        )
+    } else { return [] }
+}
+
+
 
 export default ListaDePosts;
