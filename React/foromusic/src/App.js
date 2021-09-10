326d7073dd20a701bd124f40afcc2673d1ff6837
Tadeo Cuchman <sifra@192.168.1.4>
2021-09-10 17:26:52 -0300

    se agregan css y se elige en que paginas mostrar aside

--- React/foromusic/src/App.js
+++ React/foromusic/src/App.js
@@ -7,6 +7,13 @@ import './Components/Header.css'
 import './Components/Nav.css'
 import './Components/Popup.css'
 import './Pages/Main.css'
+import './Components/Searcher.css'
+import './Pages/MyPosts.css'
+import './Components/ListaDePosts.css'
+import './Pages/Category.css'
+import './Pages/Settings.css'
+import './Pages/Profile.css'
+import './Pages/Post.css'
 
 import ScrollToTop from './Components/ScrollToTop';
 import Nav from './Components/Nav'
@@ -24,6 +31,11 @@ import Comunity from './Pages/Comunity';
 import Profile from './Pages/Profile';
 import AboutUs from './Pages/AboutUs'
 import Contact from './Pages/Contact';
+import Settings from './Pages/Settings'
+import MyPosts from './Pages/MyPosts'
+import AllUsers from './Pages/AllUsers'
+import ByDates from './Pages/ByDates'
+import Post from './Pages/Post'
 
 import { useState } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -36,37 +48,73 @@ const App = () => {
       <Router>
         <ScrollToTop />
         <Nav token= { token } changeToken= { changeToken } />
-        <Aside/>
         <Header/>
           <Switch>
             <Route exact path='/'>
               <Home />
             </Route>
+
             <Route path='/Forum'>
+              {token && <Aside/>}
               <Forum />
             </Route>
+
+            <Route path='/Post/:id'>
+              {token && <Aside/>}
+              <Post />
+            </Route>
+
             <Route path='/Category'>
+              {token && <Aside/>}
               <Category />
             </Route>
+            
+
+            <Route path='/Dates' >
+              {token && <Aside/>}
+              <ByDates />
+            </Route>
+
             <Route path='/Login'>
               <Login changeToken= { changeToken }/>
             </Route>
+
             <Route path='/Register'>
               <Register />
             </Route>
+
             <Route path='/Comunity'>
-              <Comunity />
+            <Aside/>
+              {token && <Aside/> && <Comunity />}
             </Route>
+
+            <Route path='/AllUsers'>
+            <Aside/>
+              {token && <Aside/> && <AllUsers />}
+            </Route>
+
             <Route path='/Profile'>
-              <Profile />     
+              {token && <Aside/> && <Profile />} 
             </Route>
+
+            <Route path='/Settings'>
+            {token && <Aside/> && <Settings />}
+            </Route>
+
+            <Route path='/MyPosts'>
+              {token && <Aside/> && <MyPosts />}
+            </Route>
+
             <Route path='/Contact'>
               <Contact />
             </Route>
+
             <Route path='/AboutUs'>
               <AboutUs />
             </Route>
+
           </Switch>
+          <ScrollToTop />
         <Footer/>
       </Router>
     </div>
