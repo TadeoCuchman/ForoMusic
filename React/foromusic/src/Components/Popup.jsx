4c331d5f7d00f830eddb46e120f588acf33afd64
Tadeo Cuchman <sifra@192.168.1.4>
2021-09-10 19:05:17 -0300

    agregar alerta de post succesfull

--- React/foromusic/src/Components/Popup.jsx
+++ React/foromusic/src/Components/Popup.jsx
@@ -11,8 +11,6 @@ const Popup = (props) => {
     const [album_date, changeAlbumDate] = useState('')
     const firm = localStorage.alias
 
-    
-
 
     const posting = () => {
         const postBody = {
@@ -32,12 +30,21 @@ const Popup = (props) => {
                      "auth-token" : localStorage.getItem("jwt")
               },
               body: JSON.stringify(postBody)
-       }).then(() => { props.cargarPosts() })
+        }).then(function(respuesta) {
+            return respuesta.json()
+        }).then(function (res) {
+            if (!res.succes) {
+                alert (res.message);
+            }
+        }).then(() => { 
+            props.cargarPosts() 
+            props.setMostrar(false)
+        })
     }
 
     return (props.mostrar) ? (
         <div className='popup'>
-            <form method = 'POST' id='inputArticulo' action = 'javascript:void(0);'>
+            <form method = 'POST' id='inputArticulo' action="javascript:void(0);">
                 <button id='closePopup' onClick = {() => props.setMostrar(false)}>X</button>
                 <span>Add Your Music:</span> 
                 <input type="text" name="categoria" placeholder="Category" id='cat' onChange={ (e) => changeCategory(e.target.value) } />
@@ -46,8 +53,7 @@ const Popup = (props) => {
                 <input type="text" name="band" placeholder="Band Name" id='ban' onChange={ (e) => changeBand(e.target.value) } />
                 <input type="text"  name="descripcion" placeholder="Description" id='des' onChange={ (e) => changeDescription(e.target.value) } /> 
                 <input type="date" name='albumDate' placeholder="Album Date" id='albumDate' onChange={ (e) => changeAlbumDate(e.target.value)} />
-                <input type="submit" id="submit" onClick = { () => { 
-                    posting() } }/>
+                <input type="submit" id="submit" onClick = { () =>  posting()  }/>
             </form>
         </div>
     ) : "";
