cf44256d0aaccaa413f289c2410d9460703d5206
Tadeo Cuchman <sifra@192.168.1.4>
2021-09-11 18:49:54 -0300

    implementacion de modificar en el popup

--- React/foromusic/src/Components/Popup.jsx
+++ React/foromusic/src/Components/Popup.jsx
@@ -3,13 +3,42 @@ import { useState } from 'react';
 
 
 const Popup = (props) => {
-    const [category, changeCategory] = useState('')
-    const [link, changeLink] = useState('')
-    const [album, changeAlbum] = useState('')
-    const [band, changeBand] = useState('')
-    const [description, changeDescription] = useState('')
-    const [album_date, changeAlbumDate] = useState('')
+    const [category, changeCategory] = useState(props.category || '')
+    const [link, changeLink] = useState(props.link || '')
+    const [album, changeAlbum] = useState(props.album || '')
+    const [band, changeBand] = useState(props.band || '')
+    const [description, changeDescription] = useState(props.description || '')
+    const [album_date, changeAlbumDate] = useState(props.album_date || '')
     const firm = localStorage.alias
+
+
+    const modifyPost = (a) => {
+        const modifyBody = {
+            category,
+            link,
+            album,
+            band,
+            description,
+            album_date
+        }
+        
+        fetch(`http://localhost:4000/feed/${a}`, {
+                method: 'PUT',
+                headers: {
+                    "Content-Type" : "application/json",
+                    "auth-token" : localStorage.getItem("jwt")
+                },body: JSON.stringify(modifyBody)
+            }).then((respuesta) => {
+                return respuesta.json()
+            }).then(function (res) {
+                if (!res.succes) {
+                    alert (res.message);
+                }
+            }).then(() => { 
+                props.cargarPosts() 
+                props.setMostrar(false)
+            })
+    }
 
 
     const posting = () => {
@@ -46,14 +75,22 @@ const Popup = (props) => {
         <div className='popup'>
             <form method = 'POST' id='inputArticulo' action="javascript:void(0);">
                 <button id='closePopup' onClick = {() => props.setMostrar(false)}>X</button>
-                <span>Add Your Music:</span> 
-                <input type="text" name="categoria" placeholder="Category" id='cat' onChange={ (e) => changeCategory(e.target.value) } />
-                <input type="url" name="link" placeholder="Link" id='lin' onChange={ (e) => changeLink(e.target.value) } />
-                <input type="text" name="album" placeholder="Album Name" id='alb' onChange={ (e) => changeAlbum(e.target.value) } />
-                <input type="text" name="band" placeholder="Band Name" id='ban' onChange={ (e) => changeBand(e.target.value) } />
-                <input type="text"  name="descripcion" placeholder="Description" id='des' onChange={ (e) => changeDescription(e.target.value) } /> 
-                <input type="date" name='albumDate' placeholder="Album Date" id='albumDate' onChange={ (e) => changeAlbumDate(e.target.value)} />
-                <input type="submit" id="submit" onClick = { () =>  posting()  }/>
+                <h2>Add Your Music:</h2> 
+                <span> Category:</span> 
+                <input type="text" name="category" value={category} placeholder={'Category'} id='cat' onChange={ (e) => changeCategory(e.target.value) } />
+                <span> Link:</span>
+                <input type="url" name="link" value={link} placeholder={'Link'} id='link' onChange={ (e) => changeLink(e.target.value) } />
+                <span> Album NameBand Name:</span>
+                <input type="text" name="album" value={album} placeholder={'Album'} id='alb' onChange={ (e) => changeAlbum(e.target.value) } />
+                <span> Band Name:</span>
+                <input type="text" name="band" value={band} placeholder={'Band'} id='ban' onChange={ (e) => changeBand(e.target.value) } />
+                <span> Description:</span>
+                <input type="text"  name="descripcion" value={description} placeholder={'Description'} id='des' onChange={ (e) => changeDescription(e.target.value) } /> 
+                <span> Album Date:</span>
+                <input type="date" name='albumDate' value={album_date} placeholder={'Album Date$'}  id='albumDate' onChange={ (e) => changeAlbumDate(e.target.value)} />
+                <input type="submit" id="submit" onClick = { () => { 
+                    if (!props.postToMod){posting()} else { modifyPost(props.postToMod[0].id)} 
+                    }}/>
             </form>
         </div>
     ) : "";
